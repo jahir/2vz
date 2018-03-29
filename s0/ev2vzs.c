d87316e8012b75af7c4867b352146abcf38aae61
Jakob Hirsch <jh.github@plonk.de>
2018-03-29 16:57:19 +0200

    added "interval" option to spool every x seconds
    also, huge internal rework

--- s0/ev2vzs.c
+++ s0/ev2vzs.c
@@ -11,16 +11,17 @@
 #include <errno.h>
 #include <linux/input.h>
 #include <signal.h>
+#include <poll.h>
 
 #include "ev2vzs_ts.h"
 
 #define PROG "ev2vzs"
-#define VER "0.3.0"
+#define VER "0.4.0"
 // /path/to/spool/timestamp_uuid_value
 #define VZ_SPOOLFMT "%s%llu_%s_%g"
 
-// DEBUG
-#if 0
+//#define DEBUG
+#ifdef DEBUG
 #define DPRINT(format, args...) mylog("%s: "format, __FUNCTION__, ##args)
 #define DUMP(pre, buf, len) dump(pre, buf, len)
 #else
@@ -28,45 +29,47 @@
 #define DUMP(pre, buf, len) do { /* nothing */ } while (0)
 #endif
 
-#define DIM(vec) (sizeof(vec)/sizeof(vec[0]))
-
 typedef unsigned long long TSMS;
+#define CALC_TSMS(tv) ((TSMS) tv.tv_sec * 1000 + tv.tv_usec / 1000)
 
 // config /////////////////////////
 
 struct tariff {
-	uint16_t code; // button code (from struct input_event)
 	const char * name; // channel name (for logging)
 	const char * uuid; // VZ UUID
-	TSMS ts;  // timestamp of last post
-};
-
-struct channel {
-        struct tariff peak; // peak tariff (the only one if there's no off-peak)
-        struct tariff offpeak; // off-peak tariff (if set)
-        double val; // value per impulse
-        int * opbs; // pointer to button_state value of off-peak button
-        unsigned int act : 1; // active tariff
-        struct channel * next;
-};
-
-struct config_t {
-	char * log;
-	char * spool;
-	char * dev;
-	struct channel * chan;
+	TSMS ts;  // timestamp of latest event
+	int cnt; // accumulated counts, if interval is set (i.e., > 0), is reset to 0 after post
 };
 
 struct button {
 	uint16_t code;
 	const char * name;
 };
-
-struct button button_list[] = {
+struct button buttons[] = {
 	{ BTN_LEFT, "L" }, { BTN_RIGHT, "R" }, { BTN_MIDDLE, "M" }, { BTN_SIDE, "S" }, { BTN_EXTRA, "E" }, 
+	{ 0, NULL }
 };
-int button_state[DIM(button_list)];
-struct channel * button_channel[DIM(button_list)];
+
+struct channel {
+	union {
+		struct tariff trf[2]; // tariffs (only the first one is used if there's no off-peak)
+		struct {
+			struct tariff peak, offpeak;
+		};
+	};
+	struct button *btn_imp, *btn_trf; // buttons codes for impulse and tariff (from struct input_event)
+	double val; // value per impulse
+	unsigned int act : 1; // active tariff, 0 = peak, 1 = offpeak
+	struct channel * next;
+};
+
+struct config_t {
+	char * log;
+	char * spool;
+	char * dev;
+	int interval;
+	struct channel * chan;
+};
 
 // globals ///////////////////////
 
@@ -132,21 +135,21 @@ void * myalloc(size_t size) {
 	exit(EXIT_FAILURE);
 }
 
-int button_code(char * name, uint16_t * code) {
-	for (int i=0; i<DIM(button_list); ++i) {
-		struct button * b = &button_list[i];
-		if (strcmp(name, b->name))
-			continue;
-		*code = b->code;
-		return i;
-	}
-	return -1;
+int find_button(char * name, struct button ** btn) {
+	for (struct button * b=buttons; b->code>0; ++b)
+		if (strcmp(name, b->name) == 0) {
+			if (btn != NULL)
+				*btn = b;
+			return b->code;
+		}
+	return 0;
 }
 
-const char * CONF_SEP = " \r\n";
 #define CONFIG_ELEM(c,dest) ((c=strtok(NULL,CONF_SEP)) && (dest=strdup(c)))
 #define CONFIG_ELEM_PTR(c,dest) ((c=dest=strtok(NULL,CONF_SEP)))
 struct config_t * read_config(char * conffile, struct config_t * conf) {
+	const char * CONF_SEP = " \r\n";
+
 	memset(conf, 0, sizeof(*conf));
 	FILE * fh = fopen(conffile, "r");
 	if (!fh) {
@@ -171,6 +174,16 @@ struct config_t * read_config(char * conffile, struct config_t * conf) {
 				DPRINT("line %d: spool to '%s'", lines, conf->spool);
 			else
 				mylog("config error in line %d (spool)", lines);
+		} else if (!strcmp(c, "interval")) {
+			char *p, *endptr;
+			if (CONFIG_ELEM(c, p) && *p != '\0') {
+				conf->interval = strtol(p, &endptr, 10);
+				if (*endptr == '\0')
+					DPRINT("line %d: spool interval %ds", lines, conf->interval);
+				else
+					mylog("config error in line %d (interval)", lines);
+			} else
+				mylog("config error in line %d (spool)", lines);
 		} else if (!strcmp(c, "device")) {
 			if (CONFIG_ELEM(c, conf->dev))
 				DPRINT("line %d: device path '%s'", lines, conf->dev);
@@ -179,28 +192,28 @@ struct config_t * read_config(char * conffile, struct config_t * conf) {
 		} else if (!strcmp(c, "button")) {
 			struct channel * ch = myalloc(sizeof(struct channel));
 			char *button, *val;
-			if (CONFIG_ELEM_PTR(c, button) && button_code(button, &ch->peak.code) >= 0 &&
+			if (CONFIG_ELEM_PTR(c, button) && find_button(button, &ch->btn_imp) &&
 			    CONFIG_ELEM(c, ch->peak.name) && CONFIG_ELEM(c, ch->peak.uuid) &&
 			    CONFIG_ELEM_PTR(c, val) && (ch->val = atof(val)))
 			{
-				DPRINT("line %d: button %s (0x%03hx) name %s uuid %s val %g", lines, button, ch->peak.code, ch->peak.name, ch->peak.uuid, ch->val);
+				DPRINT("line %d: button %s (0x%03hx) name %s uuid %s val %g", lines, button, ch->btn_imp->code, ch->peak.name, ch->peak.uuid, ch->val);
 				++chans;
 				*ch0 = ch;
 				ch0 = &ch->next;
-				if (CONFIG_ELEM_PTR(c, button)) { // off-peak tariff given
-					int bi; // button index
-					if ((bi = button_code(button, &ch->offpeak.code)) >= 0 &&
+				if (CONFIG_ELEM_PTR(c, button)) { // off-peak tariff given, get tariff switch button
+					if (find_button(button, &ch->btn_trf) &&
 					    CONFIG_ELEM(c, ch->offpeak.name) && CONFIG_ELEM(c, ch->offpeak.uuid))
 					{
-						if (button_channel[bi]) {
+						/*if (b->channel) {
 							mylog("config error in line %d (button): button %s is already used by channel %s/%s",
-								lines, button, button_channel[bi]->peak.name, button_channel[bi]->offpeak.name);
+								lines, button, b->channel->peak.name, b->channel->offpeak.name);
 						} else {
-							button_channel[bi] = ch;
-							ch->opbs = &button_state[bi];
+							b->channel = ch;
 							DPRINT("line %d:   off-peak button %s (0x%03hx) name %s uuid %s", 
 								lines, button, ch->offpeak.code, ch->offpeak.name, ch->offpeak.uuid);
-						}
+						}*/
+						DPRINT("line %d:   off-peak button %s (0x%03hx) name %s uuid %s", 
+							lines, button, ch->btn_trf->code, ch->offpeak.name, ch->offpeak.uuid);
 					} else {
 						mylog("config error in line %d (button off-peak)", lines);
 					}
@@ -220,15 +233,15 @@ struct config_t * read_config(char * conffile, struct config_t * conf) {
 //////////////////////////////////
 
 #define button_pressed(p, n) !!(p[n>>3] & (1u << (n&7)))
-void read_button_states() {
-	uint8_t keys[(KEY_MAX<<3)+1];
+void update_tariff_states() {
+	uint8_t keys[(KEY_MAX>>3)+1];
 	memset(keys, 0, sizeof(keys));
 	ioctl(dev_fd, EVIOCGKEY(sizeof(keys)), keys);
-	for (int i=0; i<DIM(button_state); ++i) {
-		int code = button_list[i].code;
-		button_state[i] = button_pressed(keys, code);
-		DPRINT("button_state %d (%s): %d", i, button_list[i].name, button_state[i]);
-	}
+	for (struct channel *ch=conf.chan; ch; ch=ch->next)
+		if (ch->btn_trf) {
+			ch->act = button_pressed(keys, ch->btn_trf->code);
+			DPRINT("button %s (used for channels %s/%s) state: %d", ch->btn_trf->name, ch->peak.name, ch->offpeak.name, ch->act);
+		}
 }
 
 void reopen_device() {
@@ -241,7 +254,7 @@ void reopen_device() {
 	
 	while (1) {
 		mylog("opening %s", dev_path);
-		dev_fd = open(dev_path, O_RDWR);
+		dev_fd = open(dev_path, O_RDWR | (conf.interval > 0 ? O_NONBLOCK : 0) );
 		if (dev_fd >= 0)
 			break;
 		mylog("could not open %s: %s", dev_path, strerror(errno));
@@ -266,7 +279,7 @@ void reopen_device() {
 	}
 	mylog("device: %s on %s", devname, phys);
 
-	read_button_states();
+	update_tariff_states();
 }
 
 
@@ -277,7 +290,7 @@ double tv_diff(struct timeval * tv1, struct timeval * tv2) {
 		return (tv2->tv_sec - tv1->tv_sec) - (tv1->tv_usec - tv2->tv_usec)/1e6;
 }
 
-void vzspool(unsigned long long tsms, const char * uuid, const double val) {
+void vzspool(TSMS tsms, const char * uuid, const double val) {
 	char spoolfile[256];
 	snprintf(spoolfile, sizeof(spoolfile), VZ_SPOOLFMT, conf.spool, tsms, uuid, val);
 	int fd = open(spoolfile, O_CREAT|O_EXCL|O_WRONLY, S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH);
@@ -307,81 +320,135 @@ int main(int argc, char* argv[])
 	mylog("%s %s (spool dir %s)", argv[0], VER, conf.spool);
 	mylog("source ts: %s  compile ts: %s", SOURCE_TS, COMPILE_TS);
 
-        { // install signal handlers
-                struct sigaction action;
-                memset(&action, 0, sizeof(struct sigaction));
-                action.sa_handler = handle_sig;
-                sigaction(SIGTERM, &action, NULL);
-                sigaction(SIGQUIT, &action, NULL);
-                sigaction(SIGINT, &action, NULL);
-                sigaction(SIGHUP, &action, NULL);
-        }
+	{ // install signal handlers
+		struct sigaction action;
+		memset(&action, 0, sizeof(struct sigaction));
+		action.sa_handler = handle_sig;
+		sigaction(SIGTERM, &action, NULL);
+		sigaction(SIGQUIT, &action, NULL);
+		sigaction(SIGINT, &action, NULL);
+		sigaction(SIGHUP, &action, NULL);
+	}
 
-        /* start main task */
+	/* start main task */
 	reopen_device();
 
+	time_t next_spool_time = 0;
+	if (conf.interval > 0)
+		next_spool_time = time(NULL) / conf.interval * conf.interval + conf.interval;
+	int ready = 1;
 	while (1) {
-		struct input_event ev;
-		ssize_t rc = read(dev_fd, &ev, sizeof(ev));
-		if (rc <= 0) {
-			if (rc < 0)
-				mylog("read: %s (%d)", strerror(errno), errno);
-			else
-				mylog("read: EOF");
-			reopen_device();
-			continue;
-		}
-
-		if (ev.type != EV_KEY) // mouse button pressed?
-			continue;
-
-		TSMS tsms = (TSMS) ev.time.tv_sec * 1000 + ev.time.tv_usec / 1000;
-
-		// update button states		 
-		for (int i=0; i<DIM(button_state); ++i) {
-			if (ev.code == button_list[i].code) {
-				button_state[i] = ev.value;
-				DPRINT("button %d (%s) state changed to %d", i, button_list[i].name, ev.value);
-				if (button_channel[i] && button_channel[i]->act != ev.value) {
-					struct channel * ch = button_channel[i];
-					struct tariff * trf  = *(ch->opbs) ? &ch->offpeak : &ch->peak;
-					struct tariff * trf2 = *(ch->opbs) ? &ch->peak : &ch->offpeak;
-					mylog("tariff switch: %s -> %s", trf2->name, trf->name);
-					if (trf2->ts)
-						vzspool(trf2->ts, trf->uuid, 0.0);
-					vzspool(tsms, trf2->uuid, 0.0);
-					ch->act = ev.value;
+		if (conf.interval > 0) {
+			// calculate next interval
+			struct timeval tv = {0, 0};
+			struct pollfd fds[1] = { {dev_fd, POLLIN, 0} };
+			int timeout;
+			if (gettimeofday(&tv, NULL) != 0) {
+				mylog("WARNING: gettimeofday error: %s (%d)", strerror(errno), errno);
+				timeout = 1000; // fallback poll timeout. don't know how this should happen
+			} else {
+				timeout = (next_spool_time - tv.tv_sec) * 1000 - tv.tv_usec / 1000;
+				//if (timeout < 0)
+				//	timeout = (conf.interval - (tv.tv_sec % conf.interval)) * 1000 - tv.tv_usec / 1000;
+			}
+//			if (tv.tv_sec < next_spool_time) {
+			if (timeout > 0) {
+				DPRINT("waiting for event, timeout %dms", timeout);
+				ready = poll(fds, 1, timeout);
+				if (ready < 0)
+					mylog("poll error: %s (%d)", strerror(errno), errno);
+				time(&tv.tv_sec);
+				tv.tv_usec = 0;
+			} else {
+				ready = 0;
+			}
+			if (tv.tv_sec >= next_spool_time) { // hammertime!
+				DPRINT("interval ended, spooling now");
+				for (struct channel * ch = conf.chan; ch; ch=ch->next) {
+					struct tariff * trf  = &ch->peak;
+					if (trf->cnt > 0) {
+						DPRINT("spooled channel %s UUID %s ts %lld cnt %d", trf->name, trf->uuid, trf->ts, trf->cnt);
+						vzspool(trf->ts, trf->uuid, ch->val * trf->cnt);
+						trf->cnt = 0;
+					}
+					trf = &ch->offpeak;
+					if (trf->cnt > 0) {
+						DPRINT("spooled offpeak channel %s UUID %s ts %lld cnt %d", trf->name, trf->uuid, trf->ts, trf->cnt);
+						vzspool(trf->ts, trf->uuid, ch->val * trf->cnt);
+						trf->cnt = 0;
+					}
 				}
-				break;
+				next_spool_time += conf.interval;
+#ifdef DEBUG
+				char timebuf[32];
+				strftime(timebuf, sizeof(timebuf), "%F %T", localtime(&next_spool_time)); // 2012-10-01 18:13:45
+				DPRINT("spooling finished, next spool time: %ld (%s)", next_spool_time, timebuf);
+#endif
 			}
 		}
 
-		if  (ev.value != 1) // button released
-			continue;
-		// find channel
-		struct channel * ch;
-		for (ch = conf.chan; ch; ch=ch->next) {
-			if (ev.code == ch->peak.code)
+		while (ready > 0) { // read loop 
+			struct input_event ev;
+			ssize_t rc = read(dev_fd, &ev, sizeof(ev));
+			if (rc == 0) {
+				mylog("read: EOF??");
+				reopen_device();
 				break;
-		}
-		if (!ch)
-			continue;
+			} else if (rc < 0) {
+				if (errno != EAGAIN) {
+					mylog("read error: %s (%d)", strerror(errno), errno);
+					reopen_device();
+				} else
+					DPRINT("read EAGAIN");
+				break;
+			}
 
-		char msg[64];
+			// for reference, struct input_event for mouse events:
+			// type: EV_SYN EV_KEY EV_MSC
+			// with type==EV_KEY:
+			// code: BTN_LEFT BTN_RIGHT BTN_MIDDLE ...
+			// value: 0 => released, 1 => pressed (and 2 => autorepeat)
 
-		struct tariff * trf = ch->opbs && *(ch->opbs) ? &ch->offpeak : &ch->peak;
-		if (trf->ts) {
-			TSMS tdiff = tsms - trf->ts;
-			double p = (3600.0 * 1000) * ch->val / tdiff;
-			snprintf(msg, sizeof(msg), "tdiff %6llu ms   P %8.3f W", tdiff, p);
-		} else {
-			snprintf(msg, sizeof(msg), "first impulse");
-		}
-		trf->ts = tsms;
+			if (ev.type != EV_KEY) { // mouse button event?
+				DPRINT("ignoring event type %d (code 0x%x value 0x%x)", ev.type, ev.code, ev.value);
+				continue;
+			} else
+				DPRINT("handling event type %d (code 0x%x value 0x%x)", ev.type, ev.code, ev.value);
 
-		mylog_ts(&ev.time, "%-13s: %s", trf->name, msg);
+			for (struct channel * ch = conf.chan; ch; ch=ch->next) {
+				if (ch->btn_trf && ch->btn_trf->code == ev.code) { // tariff button changed? then switch tariff
+					if (ch->act != ev.value) {
+						ch->act = (ev.value != 0); // normalize to 0 and 1
+						struct tariff * trf_cur = &ch->trf[ch->act];
+						struct tariff * trf_oth = &ch->trf[!ch->act];
+						mylog("tariff switch: %s -> %s", trf_oth->name, trf_cur->name);
+						if (trf_oth->ts)
+							vzspool(trf_oth->ts, trf_cur->uuid, 0.0); // send 0-val with last timestamp of previous tariff for _current_ tariff
+						vzspool(CALC_TSMS(ev.time), trf_oth->uuid, 0.0); // send 0-val with current timestamp for _previous_ tariff
+					} else
+						mylog("Warning: tariff button %s event (%d) without state change", ch->btn_trf->name, ev.value);
+					break;
+				} // tariff button
+				if (ev.value == 1 && ch->btn_imp && ch->btn_imp->code == ev.code) { // impulse for channel
+					struct tariff * trf = &ch->trf[ch->act];
+					TSMS tsms = (TSMS) ev.time.tv_sec * 1000 + ev.time.tv_usec / 1000;
+					if (trf->ts) {
+						TSMS tdiff = tsms - trf->ts;
+						double p = (3600.0 * 1000) * ch->val / tdiff;
+						mylog_ts(&ev.time, "%-13s: tdiff %6llu ms   P %8.3f W", trf->name, tdiff, p);
+					} else {
+						mylog_ts(&ev.time, "%-13s: first impulse", trf->name);
+					}
+					trf->ts = tsms;
 
-		vzspool(tsms, trf->uuid, ch->val);
-	}
-}
+					if (conf.interval > 0)
+						++(trf->cnt);
+					else
+						vzspool(tsms, trf->uuid, ch->val);
+					break;
+				} // impulse
+			} // find channel for button
+		} // while ready and something was read
+	} // loop forever
+} // main
 
